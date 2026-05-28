7734b07c62e87a4e8cef20140a198fa033b59777
kaxadeveloper <154353756+kaxadeveloper@users.noreply.github.com>
2026-05-28 13:12:38 +0400

    add style

--- src/layout/Navbar.jsx
+++ src/layout/Navbar.jsx
@@ -16,11 +16,14 @@ export const Navbar = () => {
                 <div className="flex items-center gap-1">
                     <div className="glass rounded-full px-2 py-1 flex items-center gap-1">
                         {navLinks.map((link, index) => (
-                            <a href={link.href} key={index}>{link.label}
+                            <a href={link.href} key={index} className="px-4 py-2 text-sm text-muted-foreground hover:text-foreground rounded-full hover:bg-surface" >
+                                {link.label}
                             </a>
                         ))}
                     </div>
                 </div>
+
+                
             </nav>
         </header>
     )
