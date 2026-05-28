412904aea7b2282458d13af698feabeed18b33f3
kaxadeveloper <154353756+kaxadeveloper@users.noreply.github.com>
2026-05-28 12:33:40 +0400

    add hover to a

--- src/layout/Navbar.jsx
+++ src/layout/Navbar.jsx
@@ -9,7 +9,7 @@ export const Navbar = () => {
     return (
         <header className="fixed top-0 left-0 right-0 bg-transparent py-5">
             <nav className="container mx-auto px-6 flex items-center justify-between">
-                <a href="#" className="text-xl font-bold tracking-tight">
+                <a href="#" className="text-xl font-bold tracking-tight hover:text-primary">
                     PM<span>.</span>
                 </a>
 
