cec11914874e884a93aaf52b451de4efb174e814
kaxadeveloper <154353756+kaxadeveloper@users.noreply.github.com>
2026-05-28 12:16:30 +0400

    add styles to nav

--- src/layout/Navbar.jsx
+++ src/layout/Navbar.jsx
@@ -8,7 +8,7 @@ const navLinks = [
 export const Navbar = () => {
     return (
         <header className="fixed top-0 left-0 bg-transparent py-5">
-            <nav>
+            <nav className="container mx-auto px-6 flex items-center">
                 <a>
                     PM<span>.</span>
                 </a>
