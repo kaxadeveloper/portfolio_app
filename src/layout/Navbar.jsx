56338f6ca47ae960e0d77c25f31e1f5a480e6d4b
kaxadeveloper <154353756+kaxadeveloper@users.noreply.github.com>
2026-05-29 17:30:18 +0400

    add close icon to Menu button

--- src/layout/Navbar.jsx
+++ src/layout/Navbar.jsx
@@ -1,4 +1,4 @@
-import { Menu } from "lucide-react";
+import { Menu, X } from "lucide-react";
 import { Button } from "../components/Button";
 import { useState } from "react";
 
@@ -36,8 +36,9 @@ export const Navbar = () => {
                     <Button size="sm">Contact Me</Button>
                 </div>
 
-                <button className="md:hidden p-2 text-foreground" onClick={() => setIsMobileMenuOpen((prev) => !prev)}>
-                    <Menu size={24} />
+                <button className="md:hidden p-2 text-foreground" onClick={() => setIsMobileMenuOpen((prev) => !prev)}
+                >
+                    {isMobileMenuOpen ? <X size={24} /> : <Menu size={24} />}
                 </button>
             </nav>
 
