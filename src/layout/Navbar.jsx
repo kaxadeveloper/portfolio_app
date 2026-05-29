abd8511a5c648841da7df7b2bf606f4c1513a860
kaxadeveloper <154353756+kaxadeveloper@users.noreply.github.com>
2026-05-29 17:23:02 +0400

    add function to Menu button

--- src/layout/Navbar.jsx
+++ src/layout/Navbar.jsx
@@ -1,5 +1,6 @@
 import { Menu } from "lucide-react";
 import { Button } from "../components/Button";
+import { useState } from "react";
 
 const navLinks = [
     { href: "#about", label: "About" },
@@ -9,6 +10,7 @@ const navLinks = [
 ]
 
 export const Navbar = () => {
+    const [isMobileMenuOpen, setIsMobileMenuOpen] = useState(false);
     return (
         <header className="fixed top-0 left-0 right-0 bg-transparent py-5">
             <nav className="container mx-auto px-6 flex items-center justify-between">
@@ -34,25 +36,27 @@ export const Navbar = () => {
                     <Button size="sm">Contact Me</Button>
                 </div>
 
-                <button className="md:hidden p-2 text-foreground">
+                <button className="md:hidden p-2 text-foreground" onClick={() => setIsMobileMenuOpen((prev) => !prev)}>
                     <Menu size={24} />
                 </button>
             </nav>
 
-            <div className="md:hidden glass-strong">
-                <div className="container mx-auto px-6 py-6 flex flex-col gap-4">
-                    {navLinks.map((link, index) => (
-                        <a
-                            href={link.href}
-                            key={index}
-                            className="text-lg text-muted-foreground hover:text-foreground py-2"
-                        >
-                            {link.label}
-                        </a>
-                    ))}
-                    <Button size="sm">Contact Me</Button>
+            {isMobileMenuOpen && (
+                <div className="md:hidden glass-strong">
+                    <div className="container mx-auto px-6 py-6 flex flex-col gap-4">
+                        {navLinks.map((link, index) => (
+                            <a
+                                href={link.href}
+                                key={index}
+                                className="text-lg text-muted-foreground hover:text-foreground py-2"
+                            >
+                                {link.label}
+                            </a>
+                        ))}
+                        <Button size="sm">Contact Me</Button>
+                    </div>
                 </div>
-            </div>
+            )}
         </header>
     )
 }
