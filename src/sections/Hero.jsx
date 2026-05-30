cd7c2b74e20cb4e4da7da03dcd4f44c2180ae89b
kaxadeveloper <154353756+kaxadeveloper@users.noreply.github.com>
2026-05-30 08:53:35 +0400

    add selfclosing div

--- src/sections/Hero.jsx
+++ src/sections/Hero.jsx
@@ -2,7 +2,12 @@ export const Hero = () => {
     return (
         <section className="relative min-h-screen flex items-center overflow-hidden">
             <div className="absolute inset-0">
-                <img src="/hero.jpg" alt="Hero image" className="w-full h-full object-cover opacity-40" />
+                <img
+                    src="/hero.jpg"
+                    alt="Hero image"
+                    className="w-full h-full object-cover opacity-40"
+                />
+                <div className="absolute inset-0 bg-gradient-to-b from-background/20 via-background/80 to-background"/>
             </div>
         </section>
     )
