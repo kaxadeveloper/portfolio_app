ee6b91c25b078d90396510ee5e448e39f8c77076
kaxadeveloper <154353756+kaxadeveloper@users.noreply.github.com>
2026-05-30 08:45:57 +0400

    add styles to div

--- src/sections/Hero.jsx
+++ src/sections/Hero.jsx
@@ -1,7 +1,7 @@
 export const Hero = () => {
     return (
         <section className="relative min-h-screen flex items-center overflow-hidden">
-            <div>
+            <div className="absolute inset-0">
                 <img src="/hero.jpg" alt="Hero image" className="w-full h-full object-cover opacity-40" />
             </div>
         </section>
