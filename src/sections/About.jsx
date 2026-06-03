6d31a22bade5cd01cefca4e2195dc04b13561f8e
kaxadeveloper <154353756+kaxadeveloper@users.noreply.github.com>
2026-06-03 11:05:37 +0400

    add styles to span tag

--- src/sections/About.jsx
+++ src/sections/About.jsx
@@ -35,7 +35,9 @@ export const About = () => {
 
                     <div className="space-y-8">
                         <div className="animate-fade-in">
-                            <span >About Me</span>
+                            <span className="text-secondary-foreground text-sm font-medium tracking-wider uppercase">
+                                About Me
+                            </span>
                         </div>
 
                         <h2>
