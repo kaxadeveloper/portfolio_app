4d9c682e8cd2538506051b4e54010023582ffec1
kaxadeveloper <154353756+kaxadeveloper@users.noreply.github.com>
2026-06-04 11:25:54 +0400

    add projects array in the projects file

--- src/sections/Projects.jsx
+++ src/sections/Projects.jsx
@@ -1,3 +1,11 @@
+const projects = [
+    {
+        title: "Fintech Dashboard",
+        description:
+        "A comprehensive financial analytics platform with real-time data visualization, portfolio management, and AI-powered insights.",
+    }
+]
+
 export const Projects = () => {
     return <section></section>
 }
