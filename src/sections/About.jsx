d46a4a30d3683388163952b41ae656fd01f7adc9
kaxadeveloper <154353756+kaxadeveloper@users.noreply.github.com>
2026-06-04 10:54:00 +0400

    item.icon

--- src/sections/About.jsx
+++ src/sections/About.jsx
@@ -75,7 +75,7 @@ export const About = () => {
                                 style={{ animationDelay: `${(idx + 1) * 100}ms` }}
                             >
                                 <div className="w-12 h-12 rounded-xl bg-primary/10 flex items-center justify-center mb-4 hover:bg-primary/20">
-                                    <item.icon />
+                                    <item.icon className="w-6 h-6 text-primary" />
                                 </div>
                                 <h3>{item.title}</h3>
                                 <p>{item.description}</p>
