072df50e7ff3abe24774f9c9fa9362248a5f6539
kaxadeveloper <154353756+kaxadeveloper@users.noreply.github.com>
2026-06-04 12:00:23 +0400

    add styles to project component's main section tag

--- src/sections/Projects.jsx
+++ src/sections/Projects.jsx
@@ -4,6 +4,33 @@ const projects = [
         description:
             "A comprehensive financial analytics platform with real-time data visualization, portfolio management, and AI-powered insights.",
         image: "/projects/project1.webp",
+        tags: ["React", "Tailwind", "NodeJS"],
+        link: "#",
+        github: "#",
+    },
+    {
+        title: "E-Commerce Platform",
+        description:
+            "A full-featured e-commerce solution with inventory management and payment methods.",
+        image: "/projects/project2.jpeg",
+        tags: ["React", "tailwindcss", "NodeJS"],
+        link: "#",
+        github: "#",
+    },
+    {
+        title: "AI Writing Assistant",
+        description:
+            "A comprehensive financial analytics platform with real-time data visualization, portfolio management, and AI-powered insights.",
+        image: "/projects/project3.jpg",
+        tags: ["React", "tailwindcss", "NodeJS"],
+        link: "#",
+        github: "#",
+    },
+    {
+        title: "Project Management Tool",
+        description:
+            "A comprehensive financial analytics platform with real-time data visualization, portfolio management, and AI-powered insights.",
+        image: "/projects/project4.webp",
         tags: ["React", "tailwindcss", "NodeJS"],
         link: "#",
         github: "#",
@@ -11,5 +38,9 @@ const projects = [
 ];
 
 export const Projects = () => {
-    return <section></section>
+    return (
+        <section id="about" className="py-32 relative overflow-hidden">
+
+        </section>
+    )
 }
