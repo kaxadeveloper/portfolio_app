27fcc642b45ca485a9a7c5b24288bcdcd51d8055
kaxadeveloper <154353756+kaxadeveloper@users.noreply.github.com>
2026-06-04 11:36:28 +0400

    add title, description, image, tags, link an github properties to project array's object

--- src/sections/Projects.jsx
+++ src/sections/Projects.jsx
@@ -2,9 +2,13 @@ const projects = [
     {
         title: "Fintech Dashboard",
         description:
-        "A comprehensive financial analytics platform with real-time data visualization, portfolio management, and AI-powered insights.",
-    }
-]
+            "A comprehensive financial analytics platform with real-time data visualization, portfolio management, and AI-powered insights.",
+        image: "/projects/project1.webp",
+        tags: ["React", "tailwindcss", "NodeJS"],
+        link: "#",
+        github: "#",
+    },
+];
 
 export const Projects = () => {
     return <section></section>
