5264bcfc159d6e04028085763ccd1555bdf394ca
kaxadeveloper <154353756+kaxadeveloper@users.noreply.github.com>
2026-06-05 14:52:32 +0400

    add styles to ArrowUpRight icon

--- src/sections/Projects.jsx
+++ src/sections/Projects.jsx
@@ -80,7 +80,7 @@ export const Projects = () => {
                                 <div className="absolute inset-0 flex items-center justify-center gap-4 opacity-0 group-hover:opacity-100 transition-opacity duration-300">
                                     <a
                                         href={project.link} className="p-3 rounded-full glass hover:bg-primary hover:text-primary-foreground transistion-all">
-                                        <ArrowUpRight />
+                                        <ArrowUpRight className="w-5 h-5" />
                                     </a>
                                     <a>
                                         <FaGithub />
