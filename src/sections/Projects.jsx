e0cf32ee0800f24bd1004d2fd9e9d264518c045a
kaxadeveloper <154353756+kaxadeveloper@users.noreply.github.com>
2026-06-05 14:49:03 +0400

    add styles to first a tag

--- src/sections/Projects.jsx
+++ src/sections/Projects.jsx
@@ -78,7 +78,8 @@ export const Projects = () => {
                                     className="absolute inset-0 bg-gradient-to-t from-card via-card/50 to-transparent opacity-60"
                                 />
                                 <div className="absolute inset-0 flex items-center justify-center gap-4 opacity-0 group-hover:opacity-100 transition-opacity duration-300">
-                                    <a>
+                                    <a
+                                        href={project.link} className="p-3 rounded-full glass hover:bg-primary hover:text-primary-foreground transistion-all">
                                         <ArrowUpRight />
                                     </a>
                                     <a>
