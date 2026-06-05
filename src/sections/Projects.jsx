d9182da132657aa63cd74090a49fb4e14b185079
kaxadeveloper <154353756+kaxadeveloper@users.noreply.github.com>
2026-06-05 17:37:06 +0400

    add styles to ArrowUpRight icon

--- src/sections/Projects.jsx
+++ src/sections/Projects.jsx
@@ -91,7 +91,7 @@ export const Projects = () => {
                             <div className="p-6 space-y-4">
                                 <div className="flex items-start justify-between">
                                     <h3 className="text-xl font-semibold group-hover:text-primary transition-colors">{project.title}</h3>
-                                    <ArrowUpRight />
+                                    <ArrowUpRight className="w-5 h-5 text-muted-foreground group-hover:text-primary group-hover:translate-x-1 group-hover:-translate-y-1 transition-all" />
                                 </div>
                                 <p>{project.description}</p>
                                 <div>{project.tags.map((tag, tagIdx) => (
