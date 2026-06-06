57bebef4bfa30804cb89fff6f5312ef1b7e1a2fc
kaxadeveloper <154353756+kaxadeveloper@users.noreply.github.com>
2026-06-06 15:59:51 +0400

    add experience's content

--- src/sections/Experience.jsx
+++ src/sections/Experience.jsx
@@ -70,7 +70,26 @@ export const Experience = () => {
                 <div className="relative">
                     <div className="timeline-glow absolute left-0 md:left-1/2 top-0 bottom-0 w-[2px] bg-gradient-to-b from-primary/70 via-primary/30 to-transparent md:-translate-x-1/2 shadow-[0_0_25px_rgba(32,178,166,0.8)]"/>
 
-                    <div className="space-y-12"></div>
+                    <div className="space-y-12">
+                        {experiences.map((exp, idx) => (
+                            <div key={idx} >
+
+                                <div></div>
+
+                                <div>
+                                    <div>
+                                        <span>{exp.period}</span>
+                                        <h3>{exp.role}</h3>
+                                        <p>{exp.company}</p>
+                                        <p>{exp.description}</p>
+                                        <div>{exp.technologies.map((tech, techIdx) => (
+                                            <span>{tech}</span>
+                                        ))}</div>
+                                    </div>
+                                </div>
+                            </div>
+                        ))}
+                    </div>
                 </div>
             </div>
         </section>
