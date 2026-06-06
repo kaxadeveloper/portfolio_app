cb95d21c2b4ff9386a683475d0a9dfb98e3d3330
kaxadeveloper <154353756+kaxadeveloper@users.noreply.github.com>
2026-06-06 08:40:44 +0400

    add styles to button's parent div

--- src/sections/Projects.jsx
+++ src/sections/Projects.jsx
@@ -108,7 +108,7 @@ export const Projects = () => {
                     ))}
                 </div>
 
-                <div>
+                <div className="text-center mt-12 animate-fade-in animation-delay-500">
                     <AnimatedBorderButton>
                         View All Projects
                         <ArrowUpRight className="w-5 h-5" />
