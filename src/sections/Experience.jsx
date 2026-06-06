732492303fcadb3882a89ca77b9d3063e989cc7a
kaxadeveloper <154353756+kaxadeveloper@users.noreply.github.com>
2026-06-06 11:01:18 +0400

    add div

--- src/sections/Experience.jsx
+++ src/sections/Experience.jsx
@@ -47,7 +47,7 @@ export const Experience = () => {
                 className="absolute top-1/2 left-1/4 w-96 h-96 bg-primary/5 rounded-full blur-3xl -translate-y-1/2"
             />
 
-            
+            <div className="container mx-auto px-6 relative z-10"></div>
         </section>
     )
 }
