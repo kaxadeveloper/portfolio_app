44824d3bade2dd23e26b1ac3423bb1584fa8b219
kaxadeveloper <154353756+kaxadeveloper@users.noreply.github.com>
2026-06-08 08:30:44 +0400

    add contactInfo array

--- src/sections/Contact.jsx
+++ src/sections/Contact.jsx
@@ -1,3 +1,24 @@
+const contactInfo = [
+    {
+        icon: Mail,
+        label: "Email",
+        value: "CodingMaster@example.com",
+        href: "mailto:CodingMaster@example.com",
+    },
+    {
+        icon: Phone,
+        label: "Phone",
+        value: "+995 123123123",
+        href: "tel:+995 123123123",
+    },
+    {
+        icon: MapPin,
+        label: "Location",
+        value: "Tbilisi, GEO",
+        href: "#",
+    },
+];
+
 export const Contact = () => {
     return <section></section>
 }
