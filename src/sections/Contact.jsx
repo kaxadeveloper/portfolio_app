e775c1f8f4bf167f7d005143cd64681072f5fb1e
kaxadeveloper <154353756+kaxadeveloper@users.noreply.github.com>
2026-06-09 16:26:04 +0400

    add handleSubmit function to form

--- src/sections/Contact.jsx
+++ src/sections/Contact.jsx
@@ -58,7 +58,7 @@ export const Contact = () => {
 
                 <div className="grid lg:grid-cols-2 gap-12 max-w-5xl mx-auto">
                     <div className="glass p-8 rounded-3xl border border-primary/30 animate-fade-in animation-delay-300">
-                        <form className="space-y-6">
+                        <form className="space-y-6" onSubmit={handleSubmit}>
                             <div>
                                 <label
                                     htmlFor="name" className="block text-sm font-medium mb-2"
